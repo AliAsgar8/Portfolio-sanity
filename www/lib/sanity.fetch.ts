ccf69efc8376ffab8bd62c980a5d59615641a5fb
AliAsgar8 <aliasgaransari825@gmail.com>
2026-02-07 22:20:22 +0530

    refactor: apply revalidate after every 60 second

--- www/lib/sanity.fetch.ts
+++ www/lib/sanity.fetch.ts
@@ -2,4 +2,12 @@ import { client } from "./sanity.client";
 import { PAGE_QUERY, SITE_SETTINGS_QUERY } from "./sanity.queries";
 
 export const getHeader = () => client.fetch(SITE_SETTINGS_QUERY);
-export const getPage = () => client.fetch(PAGE_QUERY, { slug: "home" });
+export const getPage = () =>
+  client.fetch(
+    PAGE_QUERY,
+    { slug: "home" },
+    {
+      next: { revalidate: 60 } // 60 sec
+    }
+  );
+
